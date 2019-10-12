f95dbb4f380361e60392f304bd5c7e13bb7d7c7e
s33b00r <qubert00@live.se>
2019-10-12 14:48:28 +0200

    Started on Queen

---
+++ Chess/src/Model/ChessPieces/Queen.java
@@ -0,0 +1,26 @@
+package Model.ChessPieces;
+
+import Model.Piece;
+
+import java.util.List;
+
+public class Queen extends Piece {
+
+    public Queen(int xPos, int yPos, boolean isWhite){
+        super(xPos, yPos, isWhite, "Q");
+    }
+
+    @Override
+    public boolean canMove(int xPos, int yPos, List<Piece> allPieces) {
+        if(!isOnBoard(xPos, yPos)){
+            return false;
+        }
+        if(attackingFriendly(xPos, yPos, allPieces)){
+            return false;
+        }
+        if(isGoingThroughAPieceDiagonally(xPos, yPos, allPieces)){
+            return false;
+        }
+        return true;
+    }
+}
--- Chess/src/Model/Piece.java
+++ Chess/src/Model/Piece.java
@@ -14,7 +14,7 @@ abstract public class Piece {
 
     private String notation;
 
-    public Piece(int xPos, int yPos, boolean isWhite, String notation){
+    public Piece(int xPos, int yPos, boolean isWhite, String notation) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.isWhite = isWhite;
@@ -23,20 +23,20 @@ abstract public class Piece {
 
     public abstract boolean canMove(int xPos, int yPos, List<Piece> allPieces);
 
-    public void move(int xPos, int yPos){
+    public void move(int xPos, int yPos) {
         this.xPos = xPos;
         this.yPos = yPos;
     }
 
-    protected boolean isOnBoard(int x, int y){
+    protected boolean isOnBoard(int x, int y) {
         return x >= 0 && x < 8 &&
                 y >= 0 && y < 8;
     }
 
-    protected boolean attackingFriendly(int x, int y, List<Piece> allPieces){
-        for(Piece p : allPieces){
-            if(p.getIsWhite() == isWhite){
-                if(p.getXPos() == xPos && p.getYPos() == yPos){
+    protected boolean attackingFriendly(int x, int y, List<Piece> allPieces) {
+        for (Piece p : allPieces) {
+            if (p.getIsWhite() == isWhite) {
+                if (p.xPos == xPos && p.yPos == yPos) {
                     return true;
                 }
             }
@@ -44,16 +44,49 @@ abstract public class Piece {
         return false;
     }
 
+    //Expects that the piece is moving diagonally
+    protected boolean isGoingThroughAPieceDiagonally(int x, int y, List<Piece> allPieces){
+        int dX = xPos - x;
+        int dY = yPos - y;
+
+        if(Math.abs(dX) != Math.abs(dY)){
+            System.out.println("Invalid input!");
+            return true;
+        }
+
+        int stepDirectionX = dX / Math.abs(dX);
+        int stepDirectionY = dY / Math.abs(dY);
+        int stepsX = stepDirectionX;
+        int stepsY = stepDirectionY;
+
+        while(dX != stepsX){
+            if(getPieceAt(stepsX + xPos, stepsY + yPos, allPieces) != null){
+                return true;
+            }
+            stepsX += stepDirectionX;
+            stepsY += stepDirectionY;
+        }
+        return false;
+    }
+
+    private Piece getPieceAt(int xPos, int yPos, List<Piece> allPieces){
+        for(Piece p : allPieces){
+            if(p.xPos == xPos && p.yPos == yPos){
+                return p;
+            }
+        }
+        return null;
+    }
 
     public int getXPos() {
         return xPos;
     }
+
     public int getYPos() {
         return yPos;
     }
 
-    public boolean getIsWhite(){
+    public boolean getIsWhite() {
         return isWhite;
     }
-
 }
